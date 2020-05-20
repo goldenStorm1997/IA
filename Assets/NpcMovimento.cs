745bcb9e07be9fc76412f5f60e7f5cb31e8cbe14
goldenStorm1997 <goldenStorm1997@hotmail.com>
2020-05-19 22:18:49 -0300

    IA Minerador

---
+++ Assets/NpcMovimento.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NpcMovimento : MonoBehaviour
+{
+    
+    public Transform minerio;
+    public Transform storage;
+
+    public NavMeshAgent minerador;
+
+    bool estado;
+    // Start is called before the first frame update
+    void Start()
+    {
+        minerador.SetDestination(minerio.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "minerio")
+        {
+            minerador.SetDestination(storage.transform.position);
+            Debug.Log("coletou");
+        }
+        else if (other.gameObject.tag == "storage")
+        {
+            minerador.SetDestination(minerio.transform.position);
+            Debug.Log("depositou");
+        }
+    }
+}
